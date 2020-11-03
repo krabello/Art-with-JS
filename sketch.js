9548875385a9a510b50646652f03f5fb4988ec06
Kevin Rabello <krabello@users.noreply.github.com>
2020-11-02 22:02:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,9 +28,9 @@ function draw() {
 
   beginShape();
   for (let i = 0; i <= total_degrees; i++) {
-    noiseFactor = noise(i * 0.02, float(frameCount / 160));
-    x = center_x + radius * cos(radians(i)) * noiseFactor;
-    y = center_y + radius * sin(radians(i)) * noiseFactor;
+    let noiseFactor = noise(i * 0.02, float(frameCount / 160));
+    let x = center_x + radius * cos(radians(i)) * noiseFactor;
+    let y = center_y + radius * sin(radians(i)) * noiseFactor;
     curveVertex(x, y);
   }
   endShape(CLOSE);
